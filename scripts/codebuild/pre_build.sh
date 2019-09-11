6cacaa306ebe2520b87604589eff553c6dd4c87f
Ryan Doughty <rdought1@nd.edu>
2019-09-11 11:12:44 -0400

    codebuild deploy prebuild

--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -2,4 +2,8 @@
 magenta=`tput setaf 5`
 reset=`tput sgr0`
 
-echo "${magenta}----- TESTS -----${reset}"
+echo "${magenta}----- BUILD SETUP -----${reset}"
+stackname=${1}
+deploy_cfg="deploy.json"
+stack_json="{ \"stackname\": \"${stackname}\" }"
+echo ${stack_json} > ${deploy_cfg}
